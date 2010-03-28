cded139d2cb8872e8d0389b9f5b3b0251e97967a
Juhamatti Niemelä <iiska@iki.fi>
2010-03-28 12:29:52 +0300

    Import tags and categories with wordpress converter. - Post tags and categories are retrieved from wp_terms table and included in the YAML front matter as items 'tags' and 'categories'.

--- lib/jekyll/converters/wordpress.rb
+++ lib/jekyll/converters/wordpress.rb
@@ -15,14 +15,18 @@ module Jekyll
     # post in wp_posts that has post_status = 'publish'.
     # This restriction is made because 'draft' posts are not guaranteed to
     # have valid dates.
-    QUERY = "select post_title, post_name, post_date, post_content, post_excerpt, ID, guid from wp_posts where post_status = 'publish' and post_type = 'post'"
+    POSTS_QUERY = "select post_title, post_name, post_date, post_content, post_excerpt, ID, guid from wp_posts where post_status = 'publish' and post_type = 'post'"
+    # Wordpress stores both tags and categories in wp_terms table and
+    # wp_term_taxonomy table determines whether term is tag or category.
+    # Wp_term_relationships table maps those taxonomy items to actual posts.
+    TERMS_QUERY = "SELECT wp_terms.name FROM wp_terms, wp_term_taxonomy INNER JOIN wp_term_relationships ON (wp_term_taxonomy.term_taxonomy_id=wp_term_relationships.term_taxonomy_id) WHERE ((wp_term_relationships.object_id = %d AND wp_terms.term_id = wp_term_taxonomy.term_id AND wp_term_taxonomy.taxonomy = '%s'))"
 
     def self.process(dbname, user, pass, host = 'localhost')
       db = Sequel.mysql(dbname, :user => user, :password => pass, :host => host)
 
       FileUtils.mkdir_p "_posts"
 
-      db[QUERY].each do |post|
+      db[POSTS_QUERY].each do |post|
         # Get required fields and construct Jekyll compatible name
         title = post[:post_title]
         slug = post[:post_name]
@@ -30,6 +34,8 @@ module Jekyll
         content = post[:post_content]
         name = "%02d-%02d-%02d-%s.markdown" % [date.year, date.month, date.day,
                                                slug]
+        tags = db[TERMS_QUERY % [post[:ID],'post_tag']].map{|t|t[:name]}
+        categories = db[TERMS_QUERY % [post[:ID],'category']].map{|t|t[:name]}
 
         # Get the relevant fields as a hash, delete empty fields and convert
         # to YAML for the header
@@ -38,8 +44,10 @@ module Jekyll
            'title' => title.to_s,
            'excerpt' => post[:post_excerpt].to_s,
            'wordpress_id' => post[:ID],
-           'wordpress_url' => post[:guid]
-         }.delete_if { |k,v| v.nil? || v == ''}.to_yaml
+           'wordpress_url' => post[:guid],
+           'tags' => tags,
+           'categories' => categories
+         }.delete_if { |k,v| v.nil? || v == '' || v == []}.to_yaml
 
         # Write out the data and content to file
         File.open("_posts/#{name}", "w") do |f|
